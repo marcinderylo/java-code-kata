068fd1f4addb7e090251468a2df21154f7fb049f
Marcin Deryło <marcinderylo@gmail.com>
2013-05-19 22:36:15 +0200

    first test, passing

---
+++ src/main/java/com/blogspot/marcinderylo/kata/BowlingGame.java
@@ -0,0 +1,17 @@
+/*
+ * BowlingGame.java
+ * Date: 19.05.13
+ * Time: 22:21
+ *
+ * @author Marcin Deryło, marcinderylo@gmail.com
+ */
+package com.blogspot.marcinderylo.kata;
+
+public class BowlingGame {
+    public int score() {
+        return 0;
+    }
+
+    public void roll(int pins) {
+    }
+}
--- src/test/java/com/blogspot/marcinderylo/kata/BowlingKataTest.java
+++ src/test/java/com/blogspot/marcinderylo/kata/BowlingKataTest.java
@@ -10,4 +10,13 @@ package com.blogspot.marcinderylo.kata;
 import junit.framework.TestCase;
 
 public class BowlingKataTest extends TestCase {
+
+    public void testGutterGame() throws Exception {
+        BowlingGame game = new BowlingGame();
+        for (int i = 0; i < 20; i++) {
+            game.roll(0);
+        }
+        assertEquals("score", 0, game.score());
+    }
+
 }
