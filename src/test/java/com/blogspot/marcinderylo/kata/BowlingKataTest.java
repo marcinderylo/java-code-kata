f8e95fb3f56eab67e117a13dd58c440f7f8f7c8f
Marcin Deryło <marcinderylo@gmail.com>
2013-05-19 22:21:17 +0200

    first failing test to make sure everything is setup correctly

---
+++ src/test/java/com/blogspot/marcinderylo/kata/BowlingKataTest.java
@@ -0,0 +1,13 @@
+/*
+ * BowlingKataTest.java
+ * Date: 19.05.13
+ * Time: 22:18
+ *
+ * @author Marcin Deryło, marcinderylo@gmail.com
+ */
+package com.blogspot.marcinderylo.kata;
+
+import junit.framework.TestCase;
+
+public class BowlingKataTest extends TestCase {
+}
